17ac598c0cc187f8965b529f3cde4c0d1600510d
benjaminwand <benjamin.wand@web.de>
2019-07-03 21:48:32 +0200

    added labels for checkboxes, reformated javascript file

--- menu.js
+++ menu.js
@@ -1,37 +1,41 @@
 $(document).ready(function() {
   $(".filters :checkbox").click(function() {
-  	// filter food items
-  	if ($("input:checkbox:checked").length) {
-  		$("li").show();
-  		$("li[exclusion-category" + '*="' + $(this).val() + '-maybe"] .maybe').hide();
-  		$("input:checkbox:checked").each(function() {
-  			$("li[exclusion-category" + '*="' + $(this).val() + '-red"]').hide();
-  			$("li[exclusion-category" + '*="' + $(this).val() + '-maybe"] .maybe').show();
-  		});
-  	} else {
-  		$("li").show();
-  		$("li[exclusion-category" + '*="' + $(this).val() + '-maybe"] .maybe').hide();
-  	}
+    // filter food items
+    if ($("input:checkbox:checked").length) {
+      $("li").show();
+      $('li[exclusion-category*="' + $(this).val() + '-maybe"] .maybe').hide();
+      $("input:checkbox:checked").each(function() {
+        $('li[exclusion-category*="' + $(this).val() + '-red"]').hide();
+        $(
+          'li[exclusion-category*="' + $(this).val() + '-maybe"] .maybe'
+        ).show();
+      });
+    } else {
+      $("li").show();
+      $('li[exclusion-category*="' + $(this).val() + '-maybe"] .maybe').hide();
+    }
     // hiding chapters
-    $(".chapter").each(function (){
-    	var hiddenLiItems = $(this).children("ul").children("li").filter(function(){
-    		return $(this).css('display')!== 'none'
-    	})
-    	if (hiddenLiItems.length === 0){
-    		$(this).hide()
-    	}
-    	else {$(this).show()}
-    })
-});
+    $(".chapter").each(function() {
+      var hiddenLiItems = $(this)
+        .children("li")
+        .filter(function() {
+          return $(this).css("display") !== "none";
+        });
+      if (hiddenLiItems.length) {
+        $(this).show();
+      } else {
+        $(this).hide();
+      }
+    });
+  });
   // hide/show filter button
   $(".hidefilter").click(function() {
-  	$(".hide").hide();
-  	$(".show").show();
+    $(".hide").hide();
+    $(".show").show();
   });
   $(".showfilter").click(function() {
-  	$(".show").hide();
-  	$(".hide").show();
+    $(".show").hide();
+    $(".hide").show();
   });
-  $(".chapter").each(function (){
-  })
-}); 
+});
+
