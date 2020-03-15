0c33f3eac8f90f533a81fa521f5c7ad0a19ee7d7
benjaminwand <benjamin.wand@web.de>
2020-03-15 22:40:16 +0100

    smooth scrolling with jquery

--- menu.js
+++ menu.js
@@ -97,4 +97,23 @@ $(document).ready(function() {
       }
     });
   });
+
+  // smooth scrolling
+  $('a[href^="#"]').on("click", function(e) {
+    e.preventDefault();
+    var target = this.hash;
+    var $target = $(target);
+    $("html, body")
+      .stop()
+      .animate(
+        {
+          scrollTop: $target.offset().top
+        },
+        900,
+        "swing",
+        function() {
+          window.location.hash = target;
+        }
+      );
+  });
 });
